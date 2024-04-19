29677b8f47ab6792e90dc9432abec5bd620f00c4
drali9696 <154887727+drali9696@users.noreply.github.com>
2024-04-19 20:29:42 +0330

    Update ModelSelect.tsx

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -19,41 +19,52 @@ export const ModelSelect: FC<Props> = ({
   const { t } = useTranslation('chat');
 
   return (
+    <div style={{ display: 'none' }}>
     <div className="flex flex-col">
-      <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
-        {t('Model')}
-      </label>
+     <div style={{ display: 'none' }}>
+  <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
+    {t('Model')}
+  </label>
+</div>
+</div>
+<div style={{ display: 'none' }}>
       <div className="w-full rounded-lg border border-neutral-200 bg-transparent pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white">
-        <select
-          className="w-full bg-transparent p-2"
-          placeholder={t('Select a model') || ''}
-          value={model?.id || defaultModelId}
-          onChange={(e) => {
-            onModelChange(
-              models.find(
-                (model) => model.id === e.target.value,
-              ) as OpenAIModel,
-            );
-          }}
-        >
-          {models.map((model) => (
-            <option
-              key={model.id}
-              value={model.id}
-              className="dark:bg-[#343541] dark:text-white"
-            >
-              {model.id === defaultModelId
-                ? `Default (${model.name})`
-                : model.name}
-            </option>
-          ))}
-        </select>
+      <div style={{ display: 'none' }}>
+  <select
+    className="w-full bg-transparent p-2"
+    placeholder={t('Select a model') || ''}
+    value={model?.id || defaultModelId}
+    onChange={(e) => {
+      onModelChange(
+        models.find(
+          (model) => model.id === e.target.value,
+        ) as OpenAIModel,
+      );
+    }}
+  >
+    {models.map((model) => (
+      <option
+        key={model.id}
+        value={model.id}
+        className="dark:bg-[#343541] dark:text-white"
+      >
+        {model.id === defaultModelId
+          ? `Default (${model.name})`
+          : model.name}
+      </option>
+    ))}
+  </select>
+</div>
+</div>
+
       </div>
+      <div style={{ display: 'none' }}>
       <div className="w-full mt-3 text-left text-neutral-700 dark:text-neutral-400 flex items-center">
         <a href="https://platform.openai.com/account/usage" target="_blank" className="flex items-center">
           <IconExternalLink size={18} className={"inline mr-1"} />
           {t('View Account Usage')}
         </a>
+      </div>
       </div>
     </div>
   );
